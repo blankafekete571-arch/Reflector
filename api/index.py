16286b6870441b6c5b6c70a679de8fdeb70e3a92
blankafekete571-arch <blankafekete571@gmail.com>
2026-06-03 21:10:16 +0200

    newest

--- api/index.py
+++ api/index.py
@@ -1,14 +1,8 @@
-"""
-Vercel serverless function entry point
-"""
 import sys
 import os
 
-# Add parent directory to path to import navigate module
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-# Import and export the FastAPI app
 from navigate import app
 
-# This export is recognized by Vercel's Python builder
 __all__ = ['app']
