ad606dedf8c359f3c215620b29150fb09bb8871a
blankafekete571-arch <blankafekete571@gmail.com>
2026-06-03 21:36:05 +0200

    még egy javítás

--- api/index.py
+++ api/index.py
@@ -3,6 +3,9 @@ import os
 
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
+# Disable tracing before importing navigate
+os.environ['ENABLE_PHOENIX_TRACING'] = 'false'
+
 from navigate import app
 
 __all__ = ['app']
